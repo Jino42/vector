2877a20b2104389e29e8a4d6864f9f91e6890b8d
Jino <ntoniolo@student.42.fr>
2017-10-16 19:56:04 +0200

    secur normalize 0

--- srcs/vector_normalize.c
+++ srcs/vector_normalize.c
@@ -3,10 +3,15 @@
 t_vector		vector_get_normalize(const t_vector *vec)
 {
 	t_vector new;
+	float magnitude;
 
-	new.x = vec->x / vector_sqrt_magnitude(vec);
-	new.y = vec->y / vector_sqrt_magnitude(vec);
-	new.z = vec->z / vector_sqrt_magnitude(vec);
+	magnitude = vector_magnitude(vec);
+	if (magnitude)
+	{
+		new.x = vec->x / magnitude;
+		new.y = vec->y / magnitude;
+		new.z = vec->z / magnitude;
+	}
 	return (new);
 }
 
@@ -15,9 +20,12 @@ void		vector_normalize(t_vector *vec)
 	float magnitude;
 
 	magnitude = vector_magnitude(vec);
-	vec->x = vec->x / magnitude;
-	vec->y = vec->y / magnitude;
-	vec->z = vec->z / magnitude;
+	if (magnitude)
+	{
+		vec->x = vec->x / magnitude;
+		vec->y = vec->y / magnitude;
+		vec->z = vec->z / magnitude;
+	}
 
 	return ;
 }
